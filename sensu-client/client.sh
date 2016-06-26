695d2a2cf94cd36c9b86b91271edca35f71feb98
Matt Potter <matt.potter@avenell86.co.uk>
2016-06-26 12:26:08 +0100

    refactor:

--- sensu-client/client.sh
+++ sensu-client/client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
-IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-
-sed -i 's/IP_ADDRESS/'"$IP"'/g' /etc/sensu/conf.d/client.json
-sed -i 's/POD_NAME/'"$HOSTNAME"'/g' /etc/sensu/conf.d/client.json
+# HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
+# IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+#
+# sed -i 's/IP_ADDRESS/'"$IP"'/g' /etc/sensu/conf.d/client.json
+# sed -i 's/POD_NAME/'"$HOSTNAME"'/g' /etc/sensu/conf.d/client.json
 /opt/sensu/bin/sensu-client -d /etc/sensu/conf.d -v
